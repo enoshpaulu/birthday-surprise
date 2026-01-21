8f3b889bc9901c39406a4180d586ff435a74c35a
enoshpaulu <enoshpaul.u@gmail.com>
2026-01-21 19:40:55 +0530

    Final Test changes, updated images

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import { motion } from "framer-motion";
    🔐 MASTER SWITCH — YOU CONTROL THIS
    ===================================================== */
 const TEST_MODE = false; // 🔁 set to false for her final experience
-
-const TEST_MODE = true; // 🔁 set to false for her final experience
 const REAL_BIRTHDAY = new Date("2026-01-21T20:30:00");
 const TEST_BIRTHDAY = new Date("2026-01-21T16:11:00"); // you can change freely
 const DOB = new Date("2007-01-22T22:58:05")
